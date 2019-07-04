e773adadea96402233ef53fe25b9ceffb38a3d02
paulyang <xiy076@ucsd.edu>
2019-07-04 15:08:53 +0800

    finish extra features

--- src/TicTacToe/TicTacToe.js
+++ src/TicTacToe/TicTacToe.js
@@ -15,7 +15,10 @@ function calculateWinner(squares) {
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
+            return {
+                player: squares[a],
+                boxes: [a, b, c],
+            };
         }
     }
     return null;
@@ -24,19 +27,20 @@ function calculateWinner(squares) {
 // Component Square defined as a function
 function Square(props) {
     return (
-        <button className="square" onClick={props.onClick}>
+        <button className="square" onClick={props.onClick} style={props.style}>
             {props.value}
         </button>
     );
 }
 
 class Board extends Component {
-    renderSquare(i) {
+    renderSquare(i, win) {
         return (
             <Square
                 key={i % 3}
                 value={this.props.squares[i]}
                 onClick={() => this.props.onClick(i)}
+                style={win ? { background: 'yellow' } : { background: 'white' } }
             />
         );
     }
@@ -44,10 +48,13 @@ class Board extends Component {
     render() {
         let row, col;
         let rows = [];
+        const [ box1, box2, box3 ] = this.props.winCoord;
         for (row = 0; row < 3; row++) {
             let box = [];
             for (col = 0; col < 3; col++) {
-                box.push(this.renderSquare(col + row * 3));
+                const i = col + row * 3;
+                const win = i === box1 || i === box2 || i === box3;
+                box.push(this.renderSquare(i, win));
             }
             rows.push(<div key={row} className="board-row">{box}</div>);
         }
@@ -67,7 +74,8 @@ class Game extends Component {
                 }
             }],
             stepNumber: 0,
-            xIsNext: true
+            xIsNext: true,
+            toggle: false,
         }
     }
 
@@ -81,10 +89,14 @@ class Game extends Component {
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             history: history.concat([{
-                squares: squares
+                squares: squares,
+                coord: {
+                    col: i % 3,
+                    row: Math.floor(i / 3)
+                }
             }]),
             stepNumber: history.length,
-            xIsNext: !this.state.xIsNext
+            xIsNext: !this.state.xIsNext,
         });
     }
 
@@ -97,22 +109,29 @@ class Game extends Component {
 
     render() {
         const history = this.state.history;
+        const stepNumber = this.state.stepNumber;
         const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
+        const toggle = this.state.toggle;
+        const toggleHistory = toggle ? history.slice(0).reverse() : history.slice(0);
 
-        const moves = history.map((step, move) => {
-            const desc = move ? 'Go to move #' + move : 'Go to game start';
+        const moves = toggleHistory.map((step, move) => {
+            move = toggle ? history.length - move - 1 : move;
+            const desc = move ? 'Go to move #' + move + ' at row ' + (step.coord.row + 1) + ', col ' + (step.coord.col + 1): 'Go to game start';
             return (
                 <li key={move}>
-                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                    <button className={ stepNumber === move ? 'bold' : '' } onClick={() => this.jumpTo(move)}>{desc}</button>
                 </li>
             );
         });
 
         let status;
         if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
+            status = 'Winner: ' + winner.player;
+        } else if (stepNumber === 9){
+            status = 'Draw';
+        }
+        else{
             status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
         return (
@@ -121,10 +140,12 @@ class Game extends Component {
                     <Board
                         squares={current.squares}
                         onClick={(i) => this.handleClick(i)}
+                        winCoord={winner ? winner.boxes : [null, null, null]}
                     />
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
+                    <button onClick={() => this.setState({ toggle: !this.state.toggle })}>Toggle Moves</button>
                     <ol>{moves}</ol>
                 </div>
             </div>
